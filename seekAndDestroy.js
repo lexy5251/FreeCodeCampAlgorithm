a5bcaa0e8dab15377b5e26c45830277eb37fc940
lexy5251 <xixi5251@gmail.com>
2018-11-29 18:04:25 +0000

    seekdestroy

---
+++ seekAndDestroy.js
@@ -0,0 +1,24 @@
+// You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
+
+// Note
+// You have to use the arguments object.
+
+let destroyer = (arr, ...nums) => {
+  let obj = {};
+  let narr = [];
+  for (let i = 0; i < nums.length; i++){
+    obj[nums[i]] = true;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+     if (!obj[arr[i]]) {
+       narr.push(arr[i]);
+     }
+  }
+
+  return narr;
+
+}
+
+console.log("Input: [1, 2, 3, 1, 2, 3], 2, 3, expected: [1, 1], actual: ", destroyer([1, 2, 3, 1, 2, 3], 2, 3));
+console.log('Input: ["tree", "hamburger", 53], "tree", 53, expected: ["hamburger"], actual: ', destroyer(["tree", "hamburger", 53], "tree", 53));
