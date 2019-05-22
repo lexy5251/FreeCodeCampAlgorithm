487dbe344761b433350438f3bd19bfcee4180170
Xi <xixi5251@gmail.com>
2019-05-21 23:38:59 -0400

    same frequency function

---
+++ same.js
@@ -0,0 +1,25 @@
+// The function accepts two arrays, the function should return true if every value in the array has its corresponding value squared in the second array
+// The frequency of values must be the same.
+// same([1,2,3],[4,1,9]) true
+// same([1,2,3],[1,9]) false
+
+function same(array1, array2){
+  if(array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++){
+    let squaredIndex = array2.indexOf(array1[i]**2);
+    if (squaredIndex === -1) {
+      return false;
+    }
+    array2.splice(squaredIndex, 1);
+  }
+  return true;
+}
+
+
+
+console.log("Input: [1,2,3],[4,1,9], expected: true, actual: ",same([1,2,3],[4,1,9]));
+console.log("Input: [1,2,3],[1,9], expected: false, actual: ",same([1,2,3],[1,9]));
+console.log("Input: [1,2,1],[4,4,1], expected: false, actual: ",same([1,2,1],[4,4,1]));
+
