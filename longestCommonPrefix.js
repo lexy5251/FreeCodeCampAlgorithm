0052a617dbe0ea0284187248f41e1618b90ae148
Hongyi <hongyi@hongyis-air.lan>
2021-01-27 22:32:10 -0500

    practice

---
+++ longestCommonPrefix.js
@@ -0,0 +1,16 @@
+// Write a function to find the longest common prefix string amongst an array of strings.
+// If there is no common prefix, return an empty string "".
+
+let longestCommonPrefix = (strs) => {
+    if(strs.length == 0) return "";
+    let prefix = strs[0];
+    for(let i = 1; i < strs.length; i++) {
+        while(strs[i].indexOf(prefix) != 0) {
+            prefix = prefix.substring(0, prefix.length - 1);
+        }
+    }
+    return prefix;
+  }
+
+console.log("Input: ['flower','flow','flight'], expected: 'fl', actual: ",longestCommonPrefix(['flower','flow','flight']));
+console.log("Input: ['dog','racecar','car'], expected: '', actual: ",longestCommonPrefix(['dog','racecar','car']));
